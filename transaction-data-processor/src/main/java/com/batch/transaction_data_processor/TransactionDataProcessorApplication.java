86378e2252f316325ecc0832d1c1d6656ec2320e
mvalchev <martinvalchev1@gmail.com>
2025-11-22 23:31:18 +0200

    transactionProcessor initial state

---
+++ transaction-data-processor/src/main/java/com/batch/transaction_data_processor/TransactionDataProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.batch.transaction_data_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionDataProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionDataProcessorApplication.class, args);
+	}
+
+}
---
+++ transaction-data-processor/src/test/java/com/batch/transaction_data_processor/TransactionDataProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch.transaction_data_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionDataProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
