22a0d87e02fe01dec210318717f2ea5a2eb9e43f
mvalchev <martinvalchev1@gmail.com>
2025-11-23 12:57:49 +0200

    added Person dtos and Address dtos

---
+++ src/main/java/com/batch/transaction_data_processor/dtos/Person.java
@@ -0,0 +1,28 @@
+package com.batch.transaction_data_processor.dtos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private Long personId;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String telephone;
+    private String email;
+}
